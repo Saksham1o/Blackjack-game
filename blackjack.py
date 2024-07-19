5db5e96dac547afc56cfb329d8006b1290a9f58b
Saksham1o <sakshampatiyar26@gmail.com>
2024-07-19 20:41:07 +0530

    logic cards

--- blackjack.py
+++ blackjack.py
@@ -120,3 +120,104 @@ def show_cards(cards, hidden):
        s += '\t|________________|'
    print(s)
    print()
+
+def deal_card(deck):
+   card = random.choice(deck)
+   deck.remove(card)
+   return card, deck
+
+
+def play_blackjack(deck):
+   player_cards = []
+   dealer_cards = []
+   player_score = 0
+   dealer_score = 0
+   os.system('clear')
+
+   while len(player_cards) < 2:
+       player_card, deck = deal_card(deck)
+       player_cards.append(player_card)
+       player_score += player_card.value
+
+       # If dealt a second Ace, adjust player score
+       if len(player_cards) == 2:
+           if player_cards[0].value == 11 and player_cards[1].value == 11:
+               player_cards[0].value = 1
+               player_score -= 10
+
+       print('PLAYER CARDS: ')
+       show_cards(player_cards, False)
+       print('PLAYER SCORE = ', player_score)
+
+       input('Continue...')
+
+       dealer_card, deck = deal_card(deck)
+       dealer_cards.append(dealer_card)
+       dealer_score += dealer_card.value
+
+       # If dealt a second Ace, adjust dealer score
+       # Note: adjusts 2nd card to hide that the dealer has an Ace
+       if len(dealer_cards) == 2:
+           if dealer_cards[0].value == 11 and dealer_cards[1].value == 11:
+               dealer_cards[1].value = 1
+               dealer_score -= 10
+
+       print('DEALER CARDS: ')
+       if len(dealer_cards) == 1:
+           show_cards(dealer_cards, False)
+           print('DEALER SCORE = ', dealer_score)
+       else:
+           show_cards(dealer_cards[:-1], True)
+           print('DEALER SCORE = ', dealer_score - dealer_cards[-1].value)
+
+       input('Continue...')
+
+   if player_score == 21:
+       print('PLAYER HAS A BLACKJACK!!!!')
+       print('PLAYER WINS!!!!')
+       quit()
+   os.system('clear')
+
+   print('DEALER CARDS: ')
+   show_cards(dealer_cards[:-1], True)
+   print('DEALER SCORE = ', dealer_score - dealer_cards[-1].value)
+   print()
+   print('PLAYER CARDS: ')
+   show_cards(player_cards, False)
+   print('PLAYER SCORE = ', player_score)
+
+   while player_score < 21:
+       choice = input('Enter H to Hit or S to Stand: ').upper()
+       if len(choice) != 1 or (choice not in ['H', 'S']):
+           os.system('clear')
+           print('Invalid choice!! Try Again...')
+           continue
+
+       if choice.upper() == 'S':
+           break
+       else:
+           player_card, deck = deal_card(deck)
+           player_cards.append(player_card)
+           player_score += player_card.value
+           card_pos = 0
+
+           # If dealt an Ace, adjust score for each existing Ace in hand
+           while player_score > 21 and card_pos < len(player_cards):
+               if player_cards[card_pos].value == 11:
+                   player_cards[card_pos].value = 1
+                   player_score -= 10
+                   card_pos += 1
+               else:
+                   card_pos += 1
+          
+           if player_score > 21:
+               break
+
+           os.system('clear')
+           print('DEALER CARDS: ')
+           show_cards(dealer_cards[:-1], True)
+           print('DEALER SCORE = ', dealer_score - dealer_cards[-1].value)
+           print()
+           print('PLAYER CARDS: ')
+           show_cards(player_cards, False)
+           print('PLAYER SCORE = ', player_score)
